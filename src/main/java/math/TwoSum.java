c1e7cde9f4653b7240aa8ee23852c461bde04ca3
orangemarmalade88 <orangemarmalade88@gmail.com>
2015-12-15 13:57:13 -0800

    TwoSum

--- src/main/java/math/TwoSum.java
+++ src/main/java/math/TwoSum.java
@@ -1,4 +1,4 @@
-package research;
+package math;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -8,7 +8,7 @@ import java.util.Map;
  Given an array of integers, find two numbers such that they add up to a specific target number.
 
  The function twoSum should return indices of the two numbers such that they add up to the target,
- where index1 must be less than index2. 
+ where index1 must be less than index2.
 
  Please note that your returned answers (both index1 and index2) are not zero-based.
 
@@ -23,25 +23,18 @@ public class TwoSum {
 
 	public int[] twoSum(int[] nums, int target) {
 
-		if (nums == null || nums.length < 2) {
-			return new int[] { -1, -1 };
-		}
-
-		Map<Integer, Integer> map = new HashMap<>();
-
-		for (int i = 0; i < nums.length; i++) {
-			if (map.containsKey(target - nums[i])) {
-				return new int[] { Math.min(i + 1, map.get(target - nums[i])),
-						Math.max(i + 1, map.get(target - nums[i])) };
-			} else {
-				map.put(nums[i], i + 1);
-			}
+		Map<Integer, Integer> number_to_index_map = new HashMap<>();
+		for (int index = 0; index < nums.length; index++) {
+			int current = nums[index];
+			if (!number_to_index_map.containsKey(target - current))
+				number_to_index_map.put(current, index);
+			else
+				return new int[] {
+						number_to_index_map.get(target - current) + 1,
+						index + 1 };
 		}
 
 		return new int[] { -1, -1 };
-
 	}
-
-	// Complexity: O(N)
 
 }
