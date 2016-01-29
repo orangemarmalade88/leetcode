1086cf7dd819be5d1dca80b3e6607edbfdc1d071
orangemarmalade88 <orangemarmalade88@gmail.com>
2016-01-28 16:16:50 -0800

    LRUCache

--- src/main/java/design/LRUCache.java
+++ src/main/java/design/LRUCache.java
@@ -1,6 +1,7 @@
 package design;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 /*
@@ -105,6 +106,35 @@ public class LRUCache {
 			Node n = new Node(key, value);
 			dll.appendNode(n);
 			map.put(key, n);
+		}
+	}
+
+	public class LRUCacheSTL {
+
+		Map<Integer, Integer> map;
+		int capacity;
+
+		@SuppressWarnings("serial")
+		public LRUCacheSTL(int c) {
+			capacity = c;
+			// initial capacity, load factor, access order
+			map = new LinkedHashMap<Integer, Integer>(c, 0.75f, true) {
+				@Override
+				protected boolean removeEldestEntry(
+						Map.Entry<Integer, Integer> eldest) {
+					return size() > capacity;
+				}
+			};
+		}
+
+		public int get(int key) {
+			if (!map.containsKey(key))
+				return -1;
+			return map.get(key);
+		}
+
+		public void set(int key, int value) {
+			map.put(key, value);
 		}
 	}
 }
