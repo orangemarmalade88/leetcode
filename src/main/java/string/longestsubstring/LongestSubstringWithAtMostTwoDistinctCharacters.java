b5fce693bc5a94690d625dbd6d9a33e4c4244c97
orangemarmalade88 <orangemarmalade88@gmail.com>
2016-05-07 11:35:07 -0700

    LongestSubstringWithAtMostTwoDistinctCharacters

--- src/main/java/string/longestsubstring/LongestSubstringWithAtMostTwoDistinctCharacters.java
+++ src/main/java/string/longestsubstring/LongestSubstringWithAtMostTwoDistinctCharacters.java
@@ -1,5 +1,15 @@
 package string.longestsubstring;
 
+/*
+
+ Given a string, find the length of the longest substring T that contains at most 2 distinct characters.
+
+ For example, Given s = "eceba",
+
+ T is "ece" which its length is 3.
+
+ */
+
 public class LongestSubstringWithAtMostTwoDistinctCharacters {
 	public int lengthOfLongestSubstringTwoDistinct(String s) {
 		int[] table = new int[128];
