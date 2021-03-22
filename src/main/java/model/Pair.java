42aafc1ac8f42f0633a4ba13007767c419e4076f
orangemarmalade88 <orangemarmalade88@gmail.com>
2021-03-21 21:49:18 -0700

    1152. Analyze User Website Visit Pattern (Not done)

---
+++ src/main/java/model/Pair.java
@@ -0,0 +1,28 @@
+package model;
+
+public class Pair<K, V> {
+	K key;
+	V value;
+
+	public K getKey() {
+		return key;
+	}
+
+	public void setKey(K key) {
+		this.key = key;
+	}
+
+	public V getValue() {
+		return value;
+	}
+
+	public void setValue(V value) {
+		this.value = value;
+	}
+
+	public Pair(K key, V value) {
+		super();
+		this.key = key;
+		this.value = value;
+	}
+}
