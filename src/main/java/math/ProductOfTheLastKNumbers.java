74f1dc569e7b374294eeb2ffd14a2a2fc2d7c525
orangemarmalade88 <orangemarmalade88@gmail.com>
2021-05-28 10:35:11 +0800

    ProductOfTheLastKNumbers

--- src/main/java/math/ProductOfTheLastKNumbers.java
+++ src/main/java/math/ProductOfTheLastKNumbers.java
@@ -4,34 +4,44 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProductOfTheLastKNumbers {
+	/*
+	 * Implement the class ProductOfNumbers that supports two methods:
+	 *
+	 * 1. add(int num)
+	 *
+	 * Adds the number num to the back of the current list of numbers. 2.
+	 * getProduct(int k)
+	 *
+	 * Returns the product of the last k numbers in the current list. You can
+	 * assume that always the current list has at least k numbers. At any time,
+	 * the product of any contiguous sequence of numbers will fit into a single
+	 * 32-bit integer without overflowing.
+	 *
+	 *
+	 */
 	List<Integer> products;
 
 	public ProductOfTheLastKNumbers() {
-		products = new ArrayList<>(40000);
+		products = new ArrayList<Integer>();
 		products.add(1);
 	}
 
+	// O(1)
 	public void add(int num) {
-		if (num == 0)
+		if (num != 0) {
+			int p = num * products.get(products.size() - 1);
+			products.add(p);
+		} else {
 			products.clear();
-		else
-			products.add(products.get(products.size() - 1) * num);
+			products.add(1);
+		}
 	}
 
+	// O(1)
 	public int getProduct(int k) {
-		int n = products.size();
-		return products.get(n - 1) / products.get(n - 1 - k);
-	}
-
-	public static void main(String[] args) {
-		ProductOfTheLastKNumbers p = new ProductOfTheLastKNumbers();
-		p.add(3);
-		p.add(0);
-		p.add(2);
-		p.add(5);
-		p.add(4);
-		p.getProduct(2);
-		p.getProduct(3);
-		p.getProduct(4);
+		if (products.size() - k - 1 < 0)
+			return 0;
+		return products.get(products.size() - 1)
+				/ products.get(products.size() - k - 1);
 	}
 }
