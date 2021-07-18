c30d3e7156fd005cf587188e223431f232cafef8
orangemarmalade88 <orangemarmalade88@gmail.com>
2021-07-18 14:13:22 +0800

    FindTheCelebrity

---
+++ src/main/java/FindTheCelebrity.java
@@ -0,0 +1,45 @@
+
+public class FindTheCelebrity {
+	/*
+	 * Suppose you are at a party with n people (labeled from 0 to n - 1), and
+	 * among them, there may exist one celebrity. The definition of a celebrity
+	 * is that all the other n - 1 people know him/her, but he/she does not know
+	 * any of them.
+	 * 
+	 * Now you want to find out who the celebrity is or verify that there is not
+	 * one. The only thing you are allowed to do is to ask questions like:
+	 * "Hi, A. Do you know B?" to get information about whether A knows B. You
+	 * need to find out the celebrity (or verify there is not one) by asking as
+	 * few questions as possible (in the asymptotic sense).
+	 * 
+	 * You are given a helper function bool knows(a, b) which tells you whether
+	 * A knows B. Implement a function int findCelebrity(n). There will be
+	 * exactly one celebrity if he/she is in the party. Return the celebrity's
+	 * label if there is a celebrity in the party. If there is no celebrity,
+	 * return -1.
+	 * 
+	 * 
+	 */
+	public int findCelebrity2(int n) {
+		for (int i = 0; i < n; i++) {
+			boolean celebrity = true;
+			for (int j = 0; j < n; j++) {
+				if (!knows(j, i)) {
+					celebrity = false;
+					break;
+				}
+				if (knows(i, j) && i != j) {
+					celebrity = false;
+					break;
+				}
+			}
+			if (celebrity)
+				return i;
+		}
+		return -1;
+	}
+
+	boolean knows(int i, int j) {
+		return false;
+	}
+}
