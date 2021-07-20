99d512fa32a418c9ad8b10ade8e07a1fb894e3c2
orangemarmalade88 <orangemarmalade88@gmail.com>
2021-07-20 13:47:31 +0800

    CountGoodNodesInBinaryTree

---
+++ src/main/java/tree/CountGoodNodesInBinaryTree.java
@@ -0,0 +1,30 @@
+package tree;
+
+public class CountGoodNodesInBinaryTree {
+	/*
+	 * Given a binary tree root, a node X in the tree is named good if in the
+	 * path from root to X there are no nodes with a value greater than X.
+	 * 
+	 * Return the number of good nodes in the binary tree.
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
+	int goods = 0;
+
+	public int goodNodes(TreeNode root) {
+		dfs(root, -10000);
+		return goods;
+	}
+
+	void dfs(TreeNode root, int max_so_far) {
+		if (root == null)
+			return;
+		if (root.val >= max_so_far)
+			goods++;
+		int max = Math.max(root.val, max_so_far);
+		dfs(root.left, max);
+		dfs(root.right, max);
+	}
+}
