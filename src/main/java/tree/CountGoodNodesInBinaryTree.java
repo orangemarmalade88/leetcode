ccaf4af24f9b8ca253107726aa7e19cc823d9c61
orangemarmalade88 <orangemarmalade88@gmail.com>
2021-07-21 10:26:42 +0800

    CountGoodNodesInBinaryTree

--- src/main/java/tree/CountGoodNodesInBinaryTree.java
+++ src/main/java/tree/CountGoodNodesInBinaryTree.java
@@ -1,15 +1,17 @@
 package tree;
 
+import model.TreeNode;
+
 public class CountGoodNodesInBinaryTree {
 	/*
 	 * Given a binary tree root, a node X in the tree is named good if in the
 	 * path from root to X there are no nodes with a value greater than X.
-	 * 
+	 *
 	 * Return the number of good nodes in the binary tree.
-	 * 
-	 * 
-	 * 
-	 * 
+	 *
+	 *
+	 *
+	 *
 	 */
 	int goods = 0;
 
