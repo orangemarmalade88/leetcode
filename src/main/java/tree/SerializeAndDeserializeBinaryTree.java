1fba8fadb4d73e074077e8785de28bc816a52f2a
orangemarmalade88 <orangemarmalade88@gmail.com>
2021-07-21 10:27:35 +0800

    SerializeAndDeserializeBinaryTree

---
+++ src/main/java/tree/SerializeAndDeserializeBinaryTree.java
@@ -0,0 +1,55 @@
+package tree;
+
+import model.TreeNode;
+
+public class SerializeAndDeserializeBinaryTree {
+	/*
+	 * Serialization is the process of converting a data structure or object
+	 * into a sequence of bits so that it can be stored in a file or memory
+	 * buffer, or transmitted across a network connection link to be
+	 * reconstructed later in the same or another computer environment.
+	 * 
+	 * Design an algorithm to serialize and deserialize a binary tree. There is
+	 * no restriction on how your serialization/deserialization algorithm should
+	 * work. You just need to ensure that a binary tree can be serialized to a
+	 * string and this string can be deserialized to the original tree
+	 * structure.
+	 * 
+	 * Clarification: The input/output format is the same as how LeetCode
+	 * serializes a binary tree. You do not necessarily need to follow this
+	 * format, so please be creative and come up with different approaches
+	 * yourself.
+	 * 
+	 * 
+	 */
+	public String serialize(TreeNode root) {
+		if (root == null)
+			return "N";
+		StringBuilder sb = new StringBuilder();
+		sb.append(root.val);
+		sb.append(" ").append(serialize(root.left));
+		sb.append(" ").append(serialize(root.right));
+		return sb.toString();
+	}
+
+	int index = 0;
+	String[] parts;
+
+	// Decodes your encoded data to tree.
+	public TreeNode deserialize(String data) {
+		parts = data.split(" ");
+		return helper();
+	}
+
+	public TreeNode helper() {
+		if (parts[index].equals("N")) {
+			index++;
+			return null;
+		}
+		TreeNode n = new TreeNode(Integer.valueOf(parts[index]));
+		index++;
+		n.left = helper();
+		n.right = helper();
+		return n;
+	}
+}
