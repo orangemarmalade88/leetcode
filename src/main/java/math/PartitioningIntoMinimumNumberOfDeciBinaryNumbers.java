eccea785d543106a6ac5a94f8d4c0565f9cd71a3
orangemarmalade88 <orangemarmalade88@gmail.com>
2021-07-27 10:22:21 +0800

    PartitioningIntoMinimumNumberOfDeciBinaryNumbers

---
+++ src/main/java/math/PartitioningIntoMinimumNumberOfDeciBinaryNumbers.java
@@ -0,0 +1,22 @@
+package math;
+
+public class PartitioningIntoMinimumNumberOfDeciBinaryNumbers {
+	/*
+	 * A decimal number is called deci-binary if each of its digits is either 0
+	 * or 1 without any leading zeros. For example, 101 and 1100 are
+	 * deci-binary, while 112 and 3001 are not.
+	 * 
+	 * Given a string n that represents a positive decimal integer, return the
+	 * minimum number of positive deci-binary numbers needed so that they sum up
+	 * to n.
+	 * 
+	 * 
+	 */
+	public int minPartitions(String n) {
+		int max = 0;
+		for (char c : n.toCharArray()) {
+			max = Math.max(max, c - '0');
+		}
+		return max;
+	}
+}
