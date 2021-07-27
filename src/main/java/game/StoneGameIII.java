3107346700ee6f26fde07cdd7a9da7ae07d08627
orangemarmalade88 <orangemarmalade88@gmail.com>
2021-07-27 16:01:03 +0800

    stoneGameIII

---
+++ src/main/java/game/StoneGameIII.java
@@ -0,0 +1,54 @@
+package game;
+
+public class StoneGameIII {
+	/*
+	 * Alice and Bob continue their games with piles of stones. There are
+	 * several stones arranged in a row, and each stone has an associated value
+	 * which is an integer given in the array stoneValue.
+	 * 
+	 * Alice and Bob take turns, with Alice starting first. On each player's
+	 * turn, that player can take 1, 2 or 3 stones from the first remaining
+	 * stones in the row.
+	 * 
+	 * The score of each player is the sum of values of the stones taken. The
+	 * score of each player is 0 initially.
+	 * 
+	 * The objective of the game is to end with the highest score, and the
+	 * winner is the player with the highest score and there could be a tie. The
+	 * game continues until all the stones have been taken.
+	 * 
+	 * Assume Alice and Bob play optimally.
+	 * 
+	 * Return "Alice" if Alice will win, "Bob" if Bob will win or "Tie" if they
+	 * end the game with the same score.
+	 * 
+	 * 
+	 */
+	public String stoneGameIII(int[] stoneValue) {
+		int n = stoneValue.length;
+		int[] dp = new int[n + 1];
+		int[] sum = new int[n];
+		int s = 0;
+		for (int i = n - 1; i >= 0; i--) {
+			s += stoneValue[i];
+			sum[i] = s;
+		}
+
+		for (int i = n - 1; i >= 0; i--) {
+			int res = sum[i] - dp[i + 1];
+			if (i + 2 <= n)
+				res = Math.max(res, sum[i] - dp[i + 2]);
+			if (i + 3 <= n)
+				res = Math.max(res, sum[i] - dp[i + 3]);
+			dp[i] = res;
+		}
+
+		if (2 * dp[0] > sum[0])
+			return "Alice";
+		else if (2 * dp[0] == sum[0])
+			return "Tie";
+		else
+			return "Bob";
+
+	}
+}
