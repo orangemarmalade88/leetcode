3de7385fc8ea1bfc658131e95f3d4ffea996c7ed
orangemarmalade88 <orangemarmalade88@gmail.com>
2021-08-09 12:45:31 +0800

    125. Valid Palindrome

---
+++ src/main/java/string/ValidPalindrome.java
@@ -0,0 +1,23 @@
+package string;
+
+public class ValidPalindrome {
+	public boolean isPalindrome(String s) {
+		int slow = 0;
+		int fast = s.length() - 1;
+		while (slow < fast) {
+			while (slow < s.length() - 1
+					&& !Character.isLetterOrDigit(s.charAt(slow)))
+				slow++;
+			while (fast >= 0 && !Character.isLetterOrDigit(s.charAt(fast)))
+				fast--;
+			if (slow >= s.length() - 1)
+				return true;
+			if (Character.toLowerCase(s.charAt(slow)) != Character
+					.toLowerCase(s.charAt(fast)))
+				return false;
+			slow++;
+			fast--;
+		}
+		return true;
+	}
+}
