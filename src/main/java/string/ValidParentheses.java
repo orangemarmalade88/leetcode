c282309668334e0fccb57f82a11244cb09b9f5a5
orangemarmalade88 <orangemarmalade88@gmail.com>
2021-08-12 14:16:12 +0800

    20. Valid Parentheses

--- src/main/java/string/ValidParentheses.java
+++ src/main/java/string/ValidParentheses.java
@@ -1,5 +1,26 @@
 package string;
 
-public class ValidParentheses {
+import java.util.Deque;
+import java.util.LinkedList;
 
+public class ValidParentheses {
+	public boolean isValid(String s) {
+		Deque<Character> stack = new LinkedList<>();
+		for (char c : s.toCharArray()) {
+			if (c == '[' || c == '(' || c == '{')
+				stack.push(c);
+			else {
+				if (stack.isEmpty())
+					return false;
+				char counter = stack.pop();
+				if (counter == '[' && c != ']')
+					return false;
+				if (counter == '{' && c != '}')
+					return false;
+				if (counter == '(' && c != ')')
+					return false;
+			}
+		}
+		return stack.isEmpty();
+	}
 }
