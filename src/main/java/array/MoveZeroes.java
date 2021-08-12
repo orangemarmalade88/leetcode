6528463fb05c1e29c922c03104a00f2831855b84
orangemarmalade88 <orangemarmalade88@gmail.com>
2021-08-12 14:42:58 +0800

    283. Move Zeroes

---
+++ src/main/java/array/MoveZeroes.java
@@ -0,0 +1,17 @@
+package array;
+
+public class MoveZeroes {
+	public void moveZeroes(int[] nums) {
+		int zeros = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == 0)
+				zeros++;
+			else
+				nums[i - zeros] = nums[i];
+		}
+
+		for (int i = nums.length - zeros; i < nums.length; i++) {
+			nums[i] = 0;
+		}
+	}
+}
