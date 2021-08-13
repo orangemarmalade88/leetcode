f3b6f2461100d023cd85b1268312c1c987a9874c
orangemarmalade88 <orangemarmalade88@gmail.com>
2021-08-14 05:32:58 +0800

    541. Reverse String II

---
+++ src/main/java/string/ReverseStringII.java
@@ -0,0 +1,31 @@
+package string;
+
+public class ReverseStringII {
+	public String reverseStr(String s, int k) {
+		StringBuilder sb = new StringBuilder();
+		int i = 0;
+		int n = s.length();
+		while (i < n) {
+			if (i + k <= n) {
+				sb.append(reverse(s.substring(i, i + k)));
+			} else {
+				sb.append(reverse(s.substring(i, n)));
+				break;
+			}
+			i = i + k;
+			if (i + k <= n) {
+				sb.append(s.substring(i, i + k));
+			} else {
+				sb.append(s.substring(i, n));
+				break;
+			}
+			i = i + k;
+		}
+		return sb.toString();
+	}
+
+	public StringBuilder reverse(String s) {
+		StringBuilder sb = new StringBuilder(s);
+		return sb.reverse();
+	}
+}
