9e60a9fc97bd394dbba1bffbd57b023c6dd97dcb
orangemarmalade88 <orangemarmalade88@gmail.com>
2021-08-15 12:48:25 +0800

    57. Insert Interval

---
+++ src/main/java/interval/InsertInterval.java
@@ -0,0 +1,24 @@
+package interval;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InsertInterval {
+	public int[][] insert(int[][] intervals, int[] newInterval) {
+		List<int[]> res = new ArrayList<>();
+		int i = 0;
+		while (i < intervals.length && intervals[i][1] < newInterval[0]) {
+			res.add(intervals[i++]);
+		}
+		while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+			newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+			newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+			i++;
+		}
+		res.add(newInterval);
+		while (i < intervals.length) {
+			res.add(intervals[i++]);
+		}
+		return res.toArray(new int[0][0]);
+	}
+}
