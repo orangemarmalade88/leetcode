481f0d486d65e4752d9c0f732dc4501ee6930303
orangemarmalade88 <orangemarmalade88@gmail.com>
2021-08-17 17:24:52 +0800

    1465. Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts

---
+++ src/main/java/array/MaximumAreaofaPieceofCakeAfterHorizontalandVerticalCuts.java
@@ -0,0 +1,27 @@
+package array;
+
+import java.util.Arrays;
+
+public class MaximumAreaofaPieceofCakeAfterHorizontalandVerticalCuts {
+	public int maxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts) {
+		int maxH = 0;
+		int preH = 0;
+		Arrays.sort(horizontalCuts);
+		for (int i = 1; i < horizontalCuts.length; i++) {
+			maxH = Math.max(maxH, horizontalCuts[i] - horizontalCuts[i - 1]);
+		}
+		maxH = Math.max(maxH, horizontalCuts[0]);
+		maxH = Math.max(maxH, h - horizontalCuts[horizontalCuts.length - 1]);
+
+		int maxV = 0;
+		Arrays.sort(verticalCuts);
+		for (int i = 1; i < verticalCuts.length; i++) {
+			maxV = Math.max(maxV, verticalCuts[i] - verticalCuts[i - 1]);
+		}
+		maxV = Math.max(maxV, verticalCuts[0]);
+		maxV = Math.max(maxV, w - verticalCuts[verticalCuts.length - 1]);
+
+		long area = (long) maxH * (long) maxV;
+		return (int) (area % (1000000007));
+	}
+}
