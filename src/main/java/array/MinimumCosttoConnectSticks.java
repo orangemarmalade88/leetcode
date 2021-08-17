7cd50d0f09e15bb20ab1f3e6d8fe6f8e8b07ced0
orangemarmalade88 <orangemarmalade88@gmail.com>
2021-08-17 18:14:34 +0800

    1167. Minimum Cost to Connect Sticks

---
+++ src/main/java/array/MinimumCosttoConnectSticks.java
@@ -0,0 +1,20 @@
+package array;
+
+import java.util.PriorityQueue;
+
+public class MinimumCosttoConnectSticks {
+	public int connectSticks(int[] sticks) {
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		for (int i : sticks) {
+			pq.offer(i);
+		}
+		int cost = 0;
+		while (pq.size() > 1) {
+			int a = pq.poll();
+			int b = pq.poll();
+			cost += a + b;
+			pq.offer(a + b);
+		}
+		return cost;
+	}
+}
