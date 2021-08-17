74265244f4588e9cc8856cb7e88bbd71201a8516
orangemarmalade88 <orangemarmalade88@gmail.com>
2021-08-17 14:38:02 +0800

    LRU Cache

--- src/main/java/design/LRUCache.java
+++ src/main/java/design/LRUCache.java
@@ -6,6 +6,106 @@ import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.Map;
 
+class LRUCache {
+	int capacity;
+	int size = 0;
+
+	class Node {
+		Node prev;
+		Node next;
+		int val;
+		int key;
+
+		Node(int key, int val) {
+			this.key = key;
+			this.val = val;
+		}
+	}
+
+	Map<Integer, Node> map = new HashMap<>();
+	Node head = null;
+	Node tail = null;
+
+	private void remove(Node n) {
+		if (n.prev == null) {
+			head = n.next;
+		} else {
+			n.prev.next = n.next;
+		}
+		if (n.next == null) {
+			tail = n.prev;
+		} else {
+			n.next.prev = n.prev;
+		}
+	}
+
+	private void insert(int key, int val) {
+		Node newHead = new Node(key, val);
+		newHead.next = head;
+		if (head != null)
+			head.prev = newHead;
+		head = newHead;
+		if (tail == null)
+			tail = newHead;
+	}
+
+	public LRUCache(int capacity) {
+		this.capacity = capacity;
+	}
+
+	public int get(int key) {
+		if (!map.containsKey(key))
+			return -1;
+		Node n = map.get(key);
+		remove(n);
+		insert(n.key, n.val);
+		map.put(key, head);
+		return n.val;
+	}
+
+	public void put(int key, int value) {
+		if (!map.containsKey(key)) {
+			insert(key, value);
+			map.put(key, head);
+			size++;
+		} else {
+			Node n = map.get(key);
+			remove(n);
+			insert(key, value);
+			map.put(key, head);
+		}
+		if (size > capacity) {
+			map.remove(tail.key);
+			remove(tail);
+			size--;
+		}
+	}
+
+	public static void main(String[] args) {
+		LRUCache lru = new LRUCache(3);
+		lru.put(1, 1);
+		lru.put(2, 2);
+		lru.put(3, 3);
+		lru.put(4, 4);
+
+		lru.get(4);
+
+		lru.get(3);
+		lru.get(2);
+		lru.get(1);
+
+		lru.put(5, 5);
+		lru.get(1);
+		lru.get(2);
+		lru.get(3);
+		lru.get(4);
+		lru.get(5);
+
+		System.out.println(lru.get(1));
+
+	}
+}
+
 class LRUCache2 {
 	// Can also define a doubly linked list and store Node in hashmap
 	// Implement delete in O(1)
@@ -44,11 +144,11 @@ class LRUCache2 {
 	}
 }
 
-class LRUCache extends LinkedHashMap<Integer, Integer> {
+class LRUCache3 extends LinkedHashMap<Integer, Integer> {
 	private static final long serialVersionUID = 1L;
 	private final int capacity;
 
-	public LRUCache(int capacity) {
+	public LRUCache3(int capacity) {
 		super(capacity, 0.75F, false);
 		this.capacity = capacity;
 	}
@@ -60,16 +160,5 @@ class LRUCache extends LinkedHashMap<Integer, Integer> {
 	@Override
 	protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
 		return size() > capacity;
-	}
-
-	public static void main(String[] args) {
-		LRUCache lru = new LRUCache(2);
-		lru.put(1, 1);
-		lru.put(2, 2);
-		lru.get(2);
-		lru.get(1);
-		lru.put(3, 3);
-		System.out.println(lru.get(1));
-
 	}
 }
