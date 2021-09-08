a4d7cd205756448431585121e414cd2e2cc20db1
orangemarmalade88 <orangemarmalade88@gmail.com>
2021-09-09 07:30:43 +0800

    398. Random Pick Index

---
+++ src/main/java/array/RandomPickIndex.java
@@ -0,0 +1,25 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+public class RandomPickIndex {
+	Map<Integer, List<Integer>> map = new HashMap<>();
+	Random r = new Random();
+
+	public RandomPickIndex(int[] nums) {
+		for (int i = 0; i < nums.length; i++) {
+			if (!map.containsKey(nums[i]))
+				map.put(nums[i], new ArrayList<>());
+			map.get(nums[i]).add(i);
+		}
+	}
+
+	public int pick(int target) {
+		List<Integer> l = map.get(target);
+		return l.get(r.nextInt(l.size()));
+	}
+}
