c5cbe72760802c1a7b50a0d3c1b4a68c2e92f7ff
orangemarmalade88 <orangemarmalade88@gmail.com>
2021-09-10 19:23:46 +0800

    103. Binary Tree Zigzag Level Order Traversal

---
+++ src/main/java/tree/traversal/BinaryTreeZigzagLevelOrderTraversal.java
@@ -0,0 +1,38 @@
+package tree.traversal;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import model.TreeNode;
+
+public class BinaryTreeZigzagLevelOrderTraversal {
+	public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+		List<List<Integer>> res = new ArrayList<>();
+		if (root == null)
+			return res;
+		Queue<TreeNode> q = new LinkedList<>();
+		q.offer(root);
+		boolean r = false;
+		while (!q.isEmpty()) {
+			int size = q.size();
+			List<Integer> level = new ArrayList<>();
+			while (size-- > 0) {
+				TreeNode cur = q.poll();
+				level.add(cur.val);
+				if (cur.left != null)
+					q.offer(cur.left);
+				if (cur.right != null)
+					q.offer(cur.right);
+			}
+			if (r) {
+				Collections.reverse(level);
+			}
+			r = !r;
+			res.add(level);
+		}
+		return res;
+	}
+}
