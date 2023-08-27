624495e511571a10dbdf154e8a596b2bad8b01a3
Pinjie Huang <orangemarmalade88@gmail.com>
2023-08-27 15:39:17 +0800

    Add de/ser methods

--- src/main/java/model/TreeNode.java
+++ src/main/java/model/TreeNode.java
@@ -1,5 +1,9 @@
 package model;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 public class TreeNode {
 	public int val;
 	public TreeNode left;
@@ -7,5 +11,45 @@ public class TreeNode {
 
 	public TreeNode(int x) {
 		val = x;
+	}
+
+	public static TreeNode rdeserialize(List<String> l) {
+		// Recursive deserialization.
+		if (l.get(0).equals("null")) {
+			l.remove(0);
+			return null;
+		}
+
+		TreeNode root = new TreeNode(Integer.valueOf(l.get(0)));
+		l.remove(0);
+		root.left = rdeserialize(l);
+		root.right = rdeserialize(l);
+
+		return root;
+	}
+
+	// Decodes your encoded data to tree.
+	public static TreeNode deserialize(String data) {
+		String[] data_array = data.split(",");
+		List<String> data_list = new LinkedList<String>(
+				Arrays.asList(data_array));
+		return rdeserialize(data_list);
+	}
+
+	public static String rserialize(TreeNode root, String str) {
+		// Recursive serialization.
+		if (root == null) {
+			str += "null,";
+		} else {
+			str += String.valueOf(root.val) + ",";
+			str = rserialize(root.left, str);
+			str = rserialize(root.right, str);
+		}
+		return str;
+	}
+
+	// Encodes a tree to a single string.
+	public static String serialize(TreeNode root) {
+		return rserialize(root, "");
 	}
 }
