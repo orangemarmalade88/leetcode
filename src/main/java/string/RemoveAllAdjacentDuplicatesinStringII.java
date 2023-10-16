3d6f0fdf1d200c95d8b381b6d2e84c4305f1ff3a
Pinjie Huang <orangemarmalade88@gmail.com>
2023-10-16 14:47:21 +0800

    1209. Remove All Adjacent Duplicates in String II

---
+++ src/main/java/string/RemoveAllAdjacentDuplicatesinStringII.java
@@ -0,0 +1,28 @@
+package string;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class RemoveAllAdjacentDuplicatesinStringII {
+	public String removeDuplicates(String s, int k) {
+		Deque<int[]> stack = new ArrayDeque<>();
+		for (char c : s.toCharArray()) {
+			if (stack.isEmpty() || stack.peek()[0] != c) {
+				stack.push(new int[] { c, 1 });
+			} else {
+				int[] top = stack.pop();
+				if (top[1] < k - 1) {
+					stack.push(new int[] { c, top[1] + 1 });
+				}
+			}
+		}
+		StringBuilder sb = new StringBuilder();
+		while (!stack.isEmpty()) {
+			int[] e = stack.pop();
+			for (int i = 0; i < e[1]; i++) {
+				sb.append((char) (e[0]));
+			}
+		}
+		return sb.reverse().toString();
+	}
+}
