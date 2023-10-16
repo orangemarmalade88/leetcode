c3b3f2a89bf410c27b97bb2145f05ea3fe03eecb
Pinjie Huang <orangemarmalade88@gmail.com>
2023-10-17 07:39:49 +0800

    435. Non-overlapping Intervals

---
+++ src/main/java/interval/NonoverlappingIntervals.java
@@ -0,0 +1,20 @@
+package interval;
+
+import java.util.Arrays;
+
+public class NonoverlappingIntervals {
+	public int eraseOverlapIntervals(int[][] intervals) {
+		Arrays.sort(intervals, (a, b) -> a[1] - b[1]);
+
+		int k = Integer.MIN_VALUE;
+		int count = 0;
+		for (int i = 0; i < intervals.length; i++) {
+			if (intervals[i][0] >= k) {
+				k = intervals[i][1];
+			} else {
+				count++;
+			}
+		}
+		return count;
+	}
+}
