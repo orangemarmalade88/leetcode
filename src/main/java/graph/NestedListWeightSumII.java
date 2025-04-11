3ee6dce917b70f48560bf30c190bd0a0ea877927
orangemarmalade88 <orangemarmalade88@gmail.com>
2025-04-11 17:24:31 +0800

    Update NestedListWeightSumII.java

--- src/main/java/graph/NestedListWeightSumII.java
+++ src/main/java/graph/NestedListWeightSumII.java
@@ -8,14 +8,30 @@ import model.NestedInteger;
 
 public class NestedListWeightSumII {
     private Map<NestedInteger, Integer> map = new HashMap<>();
+    private int maxDepth = 0;
 
     public int depthSumInverse(List<NestedInteger> nestedList) {
         for (NestedInteger ni : nestedList) {
-
+            dfs(ni, 1);
         }
+        int sum = 0;
+        for (Map.Entry<NestedInteger, Integer> entry : map.entrySet()) {
+            sum += entry.getKey().getInteger()
+                    * (maxDepth - entry.getValue() + 1);
+        }
+        return sum;
     }
 
-    private void dfs() {
+    private void dfs(NestedInteger ni, int depth) {
+        if (ni.isInteger()) {
+            map.put(ni, depth);
+            maxDepth = Math.max(depth, maxDepth);
+            return;
+        } else {
+            for (NestedInteger next : ni.getList()) {
+                dfs(next, depth + 1);
+            }
+        }
     }
 
 }
