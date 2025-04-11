e4980703ee0dd3f93143c673ba135755949b250d
orangemarmalade88 <orangemarmalade88@gmail.com>
2025-04-11 17:21:27 +0800

    Create NestedListWeightSumII.java

---
+++ src/main/java/graph/NestedListWeightSumII.java
@@ -0,0 +1,21 @@
+package graph;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import model.NestedInteger;
+
+public class NestedListWeightSumII {
+    private Map<NestedInteger, Integer> map = new HashMap<>();
+
+    public int depthSumInverse(List<NestedInteger> nestedList) {
+        for (NestedInteger ni : nestedList) {
+
+        }
+    }
+
+    private void dfs() {
+    }
+
+}
