c51601cc20d903877ddd48c9de7e37dce43a69a5
Pinjie Huang <orangemarmalade88@gmail.com>
2025-04-17 17:38:27 +0800

    LastStoneWeight + Rand10Rand7

---
+++ src/main/java/array/LastStoneWeight.java
@@ -0,0 +1,22 @@
+package array;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class LastStoneWeight {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(
+                Collections.reverseOrder());
+        for (int i : stones) {
+            pq.add(i);
+        }
+        while (pq.size() > 1) {
+            int first = pq.poll();
+            int second = pq.poll();
+            if (first != second) {
+                pq.add(first - second);
+            }
+        }
+        return pq.isEmpty() ? 0 : pq.poll();
+    }
+}
--- src/main/java/design/ImplementRand10UsingRand7.java
+++ src/main/java/design/ImplementRand10UsingRand7.java
@@ -2,16 +2,11 @@ package design;
 
 public class ImplementRand10UsingRand7 {
     public int rand10() {
-        return helper(rand7());
-    }
-
-    public int helper(int r) {
-        int temp = rand7() * r; // [1 -> 7 * r]
-        int d = temp / 10;
-        if (temp <= d * 10) // [1 -> d * 10]
-            return temp % 10 + 1;
-        else // [d*10 + 1 -> 7 * r]
-            return helper(temp - d * 10);
+        int result = 100;
+        while (result > 40) {
+            result = 7 * (rand7() - 1 ) + rand7();  // [1 - 49]
+        }
+        return result % 10 + 1;
     }
 
     private int rand7() {
