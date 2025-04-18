377cdb9e97020e9e2ef0618db948b3e60a1e4fdd
Pinjie Huang <orangemarmalade88@gmail.com>
2025-04-18 10:26:44 +0800

    Create LeastNumberofUniqueIntegersafterKRemovals.java

---
+++ src/main/java/array/LeastNumberofUniqueIntegersafterKRemovals.java
@@ -0,0 +1,8 @@
+package array;
+
+public class LeastNumberofUniqueIntegersafterKRemovals {
+	public int findLeastNumOfUniqueInts(int[] arr, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+    	for ()
+    }
+}
