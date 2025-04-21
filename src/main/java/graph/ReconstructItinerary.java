29263eabd6728a91f21652329cabec4b2c3c50b4
Pinjie Huang <orangemarmalade88@gmail.com>
2025-04-21 17:57:52 +0800

    Update ReconstructItinerary.java

--- src/main/java/graph/ReconstructItinerary.java
+++ src/main/java/graph/ReconstructItinerary.java
@@ -1,5 +1,6 @@
 package graph;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -8,24 +9,26 @@ import java.util.PriorityQueue;
 
 public class ReconstructItinerary {
 
-	List<String> final_result = new LinkedList<>();
-
 	public List<String> findItinerary(List<List<String>> tickets) {
 		Map<String, PriorityQueue<String>> map = new HashMap<>();
 		for (List<String> s : tickets) {
 			map.computeIfAbsent(s.get(0), value -> new PriorityQueue<>())
 					.add(s.get(1));
 		}
-		dfs(map, "JFK");
+		List<String> final_result = new LinkedList<>();
+
+		dfs(map, "JFK", final_result);
+		Collections.reverse(final_result);
 		return final_result;
 	}
 
-	public void dfs(Map<String, PriorityQueue<String>> map, String cur) {
+	public void dfs(Map<String, PriorityQueue<String>> map, String cur,
+			List<String> final_result) {
 		PriorityQueue<String> pq = map.get(cur);
 		while (pq != null && !pq.isEmpty()) {
 			String des = pq.poll();
-			dfs(map, des);
+			dfs(map, des, final_result);
 		}
-		final_result.add(0, cur);
+		final_result.add(cur);
 	}
 }
