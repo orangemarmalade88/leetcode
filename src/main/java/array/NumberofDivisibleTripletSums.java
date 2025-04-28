4d0247320a49b0f96061d58f83c85aedc4186536
Pinjie Huang <orangemarmalade88@gmail.com>
2025-04-28 16:25:40 +0800

    Update NumberofDivisibleTripletSums.java

--- src/main/java/array/NumberofDivisibleTripletSums.java
+++ src/main/java/array/NumberofDivisibleTripletSums.java
@@ -6,12 +6,9 @@ public class NumberofDivisibleTripletSums {
 		for (int i = 0; i < nums.length; i++) {
 			int[] map = new int[d];
 			for (int j = i + 1; j < nums.length; j++) {
-				map[nums[j] % d]++;
-				for (int k = j + 1; k < nums.length; k++) {
-					int remain = (nums[i] + nums[k]) % d;
-					int remain2 = d - remain;
-					count += map[remain2];
-				}
+				int remain = d - (nums[i] + nums[j]) % d;
+				count += map[remain];
+				map[remain]++;
 			}
 		}
 		return count;
